95df44ca1d8142fb61472756aadbeeb64d38f6d1
Shahid-sk <mail2sk.shahid@gmail.com>
2021-07-17 18:31:44 +0530

    added: created class for finding maximum float value

--- src/maximum_generics/TestMaximum.java
+++ src/maximum_generics/TestMaximum.java
@@ -1,9 +1,11 @@
 package maximum_generics;
-import java.util.Scanner;
-public class TestMaximum {
-	public static Integer testMax(Integer number1, Integer number2, Integer number3) {
 
-		Integer maximumNumber = number1;
+import java.util.Scanner;
+
+public class TestMaximum {
+	public static float testMax(Float number1, Float number2, Float number3) {
+
+		Float maximumNumber = number1;
 		if (number2.compareTo(maximumNumber) > 0) {
 			maximumNumber = number2;
 		}
@@ -12,12 +14,13 @@ public class TestMaximum {
 		}
 		return maximumNumber;
 	}
+
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter x,y,z value");
-		Integer x = sc.nextInt();
-		Integer y = sc.nextInt();
-		Integer z = sc.nextInt();
+		Float x = sc.nextFloat();
+		Float y = sc.nextFloat();
+		Float z = sc.nextFloat();
 		sc.close();
 		System.out.println("maximum value is:" + testMax(x, y, z));
 	}
