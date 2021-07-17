7a7fb0fb75bc15992f69fcadfade2e544e3e69c2
Shahid-sk <mail2sk.shahid@gmail.com>
2021-07-17 19:36:49 +0530

    refactored: added generic method

--- src/maximum_generics/TestMaximum.java
+++ src/maximum_generics/TestMaximum.java
@@ -3,26 +3,38 @@ package maximum_generics;
 import java.util.Scanner;
 
 public class TestMaximum {
-	public static String testMax(String name1, String name2, String name3) {
+	public static <E extends Comparable<E>> E testMax(E x, E y, E z) {
 
-		String maximum = name1;
-		if (name2.compareTo(maximum) > 0) {
-			maximum = name2;
+		E maximum = x;
+		if (y.compareTo(maximum) > 0) {
+			maximum = y;
 		}
-		if (name3.compareTo(maximum) > 0) {
-			maximum = name3;
+		if (z.compareTo(maximum) > 0) {
+			maximum = z;
 		}
 		return maximum;
 	}
 
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter x,y,z value");
-		String x = sc.next();
-		String y = sc.next();
-		String z = sc.next();
-		sc.close();
-		System.out.println("maximum value is:" + testMax(x, y, z));
-	}
+		System.out.println("enter x,y,z values for integer data");
+		Integer a = sc.nextInt();
+		Integer b = sc.nextInt();
+		Integer c = sc.nextInt();
+		System.out.println("maximum value is:" + testMax(a, b, c));
 
+		System.out.println("enter x,y,z values for float data");
+		Float a1 = sc.nextFloat();
+		Float b1 = sc.nextFloat();
+		Float c1 = sc.nextFloat();
+		System.out.println("maximum value is:" + testMax(a1, b1, c1));
+
+		System.out.println("enter x,y,z values for string data");
+		String a2 = sc.next();
+		String b2 = sc.next();
+		String c2 = sc.next();
+		System.out.println("maximum value is:" + testMax(a2, b2, c2));
+		sc.close();
+
+	}
 }
